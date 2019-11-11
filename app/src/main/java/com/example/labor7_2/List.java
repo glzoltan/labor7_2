bf5cf08a1cbcfc912f54b4767eefb3ae05967867
glzoltan <gaborlukacszoltan@gmail.com>
2019-11-11 16:56:12 +0200

    update

--- app/src/main/java/com/example/labor7_2/List.java
+++ app/src/main/java/com/example/labor7_2/List.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,14 +24,17 @@ import com.example.labor7_2.R;
  */
 public class List extends Fragment {
 
-
+RecyclerView myrec;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View v= inflater.inflate(R.layout.fragment_list, container, false);
+        myrec = (RecyclerView)v.findViewById(R.id.recyclerview);
+
+        return v;
     }
 
 
